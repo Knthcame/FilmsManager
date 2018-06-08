920c3bca0cf3ed0dfee60d3e7cd8df20bf3e013d
Kenneth Cartañá <kenneth.cartanya@itequia.com>
2018-06-08 16:30:07 +0200

    Added IOS Dialog support. Changed AddFilm MovieGenre entry for a picker

---
+++ FilmsManager/FilmsManager/Models/GenreModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmsManager.Models
+{
+	public class GenreModel : BaseModel
+	{
+		private string _name;
+
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				_name = value;
+				RaisePropertyChanged();
+			}
+		}
+
+			public GenreModel(string name)
+		{
+			_name = name;
+		}
+	}
+}
--- FilmsManager/FilmsManager/ViewModels/AddFilmViewModel.cs
+++ FilmsManager/FilmsManager/ViewModels/AddFilmViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using FilmsManager.Models;
 using FilmsManager.Services.Interfaces;
 using FilmsManager.ViewModels.Commands;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -8,52 +9,75 @@ using Xamarin.Forms;
 
 namespace FilmsManager.ViewModels
 {
-    public class AddFilmViewModel : BaseViewModel
-    {
-        private string _movieImage = "icon.png";
-        private string _movieGenre;
-        private string _movieTitle;
+	public class AddFilmViewModel : BaseViewModel
+	{
+		private string _movieImage = "icon.png";
+		private string _movieGenre;
+		private string _movieTitle;
+		private GenreModel _selectedGenre;
 
-        public ICommand AddCommand { get; set; }
-        public ICommand OpenGalleryCommand { get; set; }
+		public ICommand AddCommand { get; set; }
+		public ICommand OpenGalleryCommand { get; set; }
 
-        public string MovieTitle
-        {
-            get => _movieTitle;
-            set
-            {
-                _movieTitle = value;
-                RaisePropertyChanged();
-            }
-        }
+		public IList<GenreModel> GenreList { get; set; } = new ObservableCollection<GenreModel>()
+		{
+			new GenreModel("Fantasy"),
+			new GenreModel("Action"),
+			new GenreModel("Drama"),
+			new GenreModel("Humour"),
+			new GenreModel("Terror"),
+			new GenreModel("ScieneFiction"),
+			new GenreModel("Super Heroes")
+		};
 
-        public string MovieGenre
-        {
-            get => _movieGenre;
-            set
-            {
-                _movieGenre = value;
-                RaisePropertyChanged();
-            }
-        }
+		public GenreModel SelectedGenre
+		{
+			get => _selectedGenre;
+			set
+			{
+				_selectedGenre = value;
+				_movieGenre = _selectedGenre.Name;
+			}
+		}
 
-        public string MovieImage
-        {
-            get => _movieImage;
-            set
-            {
-                _movieImage = value;
-                RaisePropertyChanged();
-            }
-        }
+		public string MovieTitle
+		{
+			get => _movieTitle;
+			set
+			{
+				_movieTitle = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		public string MovieGenre
+		{
+			get => _movieGenre;
+			set
+			{
+				_movieGenre = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		public string MovieImage
+		{
+			get => _movieImage;
+			set
+			{
+				_movieImage = value;
+				RaisePropertyChanged();
+			}
+		}
 
 
-        public AddFilmViewModel(ObservableCollection<MovieModel> movieList)
-        {
-            AddCommand = new AddCommand(NavigationService, this, movieList);
-            OpenGalleryCommand = new OpenGalleryCommand(NavigationService);
-            MessagingCenter.Subscribe<PickImageCommand, string>(this, "PickImage", (s, a) => MovieImage = a);
-        }
+		public AddFilmViewModel(ObservableCollection<MovieModel> movieList)
+		{
+			AddCommand = new AddCommand(NavigationService, this, movieList);
+			OpenGalleryCommand = new OpenGalleryCommand(NavigationService);
+			MessagingCenter.Subscribe<PickImageCommand, string>(this, "PickImage", (s, a) => MovieImage = a);
+			//LoadGenreList();
+		}
 
 		public virtual async Task<bool> OnBackButtonPressedAsync()
 		{
@@ -61,5 +85,17 @@ namespace FilmsManager.ViewModels
 			if (action) await NavigationService.GoBack();
 			return action;
 		}
+
+		//private void LoadGenreList()
+		//{
+		//	GenreList.Add("Fantasy");
+		//	GenreList.Add("Action");
+		//	GenreList.Add("Drama");
+		//	GenreList.Add("Humour");
+		//	GenreList.Add("Terror");
+		//	GenreList.Add("ScieneFiction");
+		//	GenreList.Add("Super Heroes");
+		//}
+
 	}
 }
