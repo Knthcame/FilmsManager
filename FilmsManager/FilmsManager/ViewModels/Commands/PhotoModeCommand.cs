01ced69a966b927136092c57b8f56a44d81f2e98
Kenneth Cartañá <kenneth.cartanya@itequia.com>
2018-06-22 12:09:27 +0200

    Fix for IOS 4

--- FilmsManager/FilmsManager/ViewModels/Commands/PhotoModeCommand.cs
+++ FilmsManager/FilmsManager/ViewModels/Commands/PhotoModeCommand.cs
@@ -1,8 +1,6 @@
 ﻿using FilmsManager.Models;
 using Plugin.Media;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
